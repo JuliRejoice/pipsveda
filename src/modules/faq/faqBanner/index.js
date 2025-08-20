b3dd57ff3ef92b3b4b14c8fa2f5bb1048d78cac1
mahesh <dev030.rejoice@gmail.com>
2025-08-20 12:39:24 +0530

    styles: created faq page design

---
+++ src/modules/faq/faqBanner/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './faqBanner.module.scss';
+export default function FaqBanner() {
+    return (
+        <div className={styles.faqBanner}>
+            <div className='container'>
+                <div className={styles.text}>
+                    <h1>
+                        Frequently Ask Question
+                    </h1>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
