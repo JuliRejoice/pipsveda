13fd8ce0649ab1efdab6480c7fa4a058a9ec9049
mahesh <dev030.rejoice@gmail.com>
2025-08-20 15:39:48 +0530

    styles:Journey

--- src/compoents/logo/index.js
+++ src/compoents/logo/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from './logo.module.scss';
+import Link from 'next/link';
 const PipsVedaLogo = '/assets/logo/logo.png';
 export default function Logo() {
   return (
-    <div className={styles.logo}>
-        <img src={PipsVedaLogo} alt='PipsVedaLogo'/>
-    </div>
+    <Link href="/" aria-label='logo'>
+      <div className={styles.logo}>
+        <img src={PipsVedaLogo} alt='PipsVedaLogo' />
+      </div>
+    </Link>
   )
 }
