cde874be345a7523a84958c71ec066f2f3ae2c05
mahesh <dev030.rejoice@gmail.com>
2025-10-07 11:29:54 +0530

    styles:some design issue fixed

--- src/modules/home/categories/index.js
+++ src/modules/home/categories/index.js
@@ -1,9 +1,69 @@
+'use client'
 import React from 'react'
-import styles from './'
-export default function Categories() {
-    return (
-        <div>
+import styles from './categories.module.scss'
+import Slider from "react-slick";
 
+
+export default function Categories() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+        ]
+
+    };
+    return (
+        <div className={styles.categoriesSectionAlignment}>
+            <div className='container'>
+                <div className={styles.title}>
+                    <h2>
+                        Explore by variety of topics
+                    </h2>
+                    <p>
+                        Choose from a wide range of focused financial caregories
+                    </p>
+                </div>
+                <Slider {...settings}>
+                    {
+                        [...Array(7)].map(() => {
+                            return (
+                                <div className={styles.box}>
+                                    <div className={styles.image}>
+                                        <img src="https://pips-veda.netlify.app/assets/images/blog-img.png" />
+                                    </div>
+                                    <div className={styles.details}>
+                                        <p>
+                                            Forex Trading
+                                        </p>
+                                        <span>112 Courses</span>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </Slider>
+            </div>
         </div>
     )
 }
--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -12,11 +12,13 @@ import JourneySection from '../journeySection'
 import GetCertified from './getCertified'
 import Insights from './insights'
 import Havequestions from './havequestions'
+import Categories from './categories'
 
 export default function HomePage() {
   return (
     <div>
       <Herobanner />
+      <Categories />
       <ExploreDifferent />
       <FinancialFreedom />
       <TelegramCommunities />
